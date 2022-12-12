8703d50936b602366bfcac8a25218e6cd0c6a835
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-12-12 12:51:43 +0100

    test relationship

--- src/main/java/org/generation/italy/pizza/demo/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/generation/italy/pizza/demo/SpringLaMiaPizzeriaCrudApplication.java
@@ -1,10 +1,14 @@
 package org.generation.italy.pizza.demo;
 
 
+import java.time.LocalDate;
+
 import org.generation.italy.pizza.demo.pojo.Drink;
 import org.generation.italy.pizza.demo.pojo.Pizza;
+import org.generation.italy.pizza.demo.pojo.Promotion;
 import org.generation.italy.pizza.demo.service.DrinkService;
 import org.generation.italy.pizza.demo.service.PizzaService;
+import org.generation.italy.pizza.demo.service.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,23 +29,32 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
 	@Autowired
 	private DrinkService drinkServ;
 	
+	@Autowired
+	private PromotionService promotionService;
+	
 	//DATI FAKER
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Pizza p1 = new Pizza("pizza1", "my p1 desc", 1000);
-		Pizza p2 = new Pizza("pizza2", "my p2 desc", 2200);
-		Pizza p3 = new Pizza("pizza3", "my p3 desc", 400);
-		Pizza p4 = new Pizza("pizza4", "my p4 desc", 1200);
+		//promotions
+		Promotion prom1 = new  Promotion(LocalDate.parse("2022-12-13"), LocalDate.parse("2022-12-31"), "rebix promotion");
+		Promotion prom2 = new  Promotion(LocalDate.parse("2022-12-13"), LocalDate.parse("2022-12-31"), "rebix promotion2");
+		
+		promotionService.save(prom1);
+		promotionService.save(prom2);
+		
+		// pizza
+		Pizza p1 = new Pizza("pizza1", "my p1 desc", 1000 , prom1);
+		Pizza p2 = new Pizza("pizza2", "my p2 desc", 2200, prom1);
+		Pizza p3 = new Pizza("pizza3", "my p3 desc", 400, prom2);
+		Pizza p4 = new Pizza("pizza4", "my p4 desc", 1200, prom2);
 	
 		pizzaServ.save(p1);
 		pizzaServ.save(p2);
 		pizzaServ.save(p3);
 		pizzaServ.save(p4);
 		
-//		List<Pizza> pizzas = pizzaServ.findAll();
-//		System.out.println(pizzas);
-		
+		// drink
 		Drink d1 = new Drink("drink 1", "desc drink 1", 10000);
 		Drink d2 = new Drink("drink 2", "desc drink 2", 12000);
 		Drink d3 = new Drink("drink 3", "desc drink 3", 1000);
@@ -51,6 +64,8 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
 		drinkServ.save(d2);
 		drinkServ.save(d3);
 		drinkServ.save(d4);
+		
+		
 		
 	}
 }
